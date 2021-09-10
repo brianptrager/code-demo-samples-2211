4f4c415525b86cb95db1665eaa4cf48d7e5d60aa
bptrager <brian.trager@rit.edu>
2021-09-10 09:32:45 -0400

    Code update

---
+++ ProgramExit/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ProgramExit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter 'a' or 'b': ");
+            string input = Console.ReadLine();
+            if(input == "a" || input == "b")
+                Console.WriteLine("You can follow instructions.");
+            else
+            {
+                Console.WriteLine("Umm, not sure what to tell you.");
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine("Thank you for using this very very very simple program.");
+        }
+    }
+}
