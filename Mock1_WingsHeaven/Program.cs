205bbe9b5d7c2c351cb7ce704c47315eb423ab94
bptrager <brian.trager@rit.edu>
2021-09-22 08:59:32 -0400

    Update 1

---
+++ Mock1_WingsHeaven/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+/* Name: Professor BT
+ * Date: 9/22/2021
+ * Course: NMAD.180: Programming Fundamentals I: Mobile Domain
+ * Program: Wings Heaven
+ * Purpose: Program to take orders for food
+ * Caveats: This makes me hungry!
+ * */
+
+namespace Mock1Exam_WingsHeaven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ MockExam1_SwapMeet/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+/* Name: Professor BT
+ * Date: 9/22/2021
+ * Course: NMAD.180: Programming Fundamentals I: Mobile Domain
+ * Program: Swap Meet
+ * Purpose: Program to buy/sell disks
+ * Caveats: I may have no clue what to do
+ * */
+
+namespace MockExam1_SwapMeet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Disk4Cheap";
+
+            Console.WriteLine("***Disk4Cheap***\n");
+            Console.Write("How many disk drives are in the current inventory?: ");
+
+            //string input = Console.ReadLine();
+            //if(int.TryParse(input, out int numInput))
+            if(int.TryParse(Console.ReadLine(), out int numInput))
+            {
+
+            }
+        }
+    }
+}
