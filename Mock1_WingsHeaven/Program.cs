79c5951b8c5349f894eb3febed2ec56fa8df257d
bptrager <brian.trager@rit.edu>
2021-09-22 09:44:37 -0400

    First menu set up

--- Mock1_WingsHeaven/Program.cs
+++ Mock1_WingsHeaven/Program.cs
@@ -13,7 +13,27 @@ namespace Mock1Exam_WingsHeaven
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Cost for each food item or combo
+            const double BONELESS_WINGS_PRICE = 7.99;
+            const double TRADITIONAL_WINGS_PRICE = 9.99;
+            const double COMBO_DISCOUNT_RATE = 0.18;
+            const double COMBO_WINGS_PRICE = (BONELESS_WINGS_PRICE + TRADITIONAL_WINGS_PRICE) - ((BONELESS_WINGS_PRICE + TRADITIONAL_WINGS_PRICE) * COMBO_DISCOUNT_RATE);
+            //sauce
+            const double BUFFALO_SAUCE_PRICE = 0.29;
+            const double MILD_SAUCE_PRICE = 0.39;
+            const double BBQ_SAUCE_PRICE = 0.29;
+            //sides
+            const double WAFFLE_FRIES_PRICE = 3.49;
+            const double ONION_RINGS_PRICE = 2.49;
+
+            Console.WriteLine("*** HEAVENLY WINGS ***\n");
+
+            Console.Write($"Type of Wings:\n\t{"1. Boneless",-20}{BONELESS_WINGS_PRICE, 9:C}" +
+                $"\t{"2. Traditional",-20}{TRADITIONAL_WINGS_PRICE,9:C}\n" +
+                $"\t{"1. Both",-20}{BONELESS_WINGS_PRICE + TRADITIONAL_WINGS_PRICE,9:C}" +
+                $"Enter your selection: ");
+
+
         }
     }
 }
