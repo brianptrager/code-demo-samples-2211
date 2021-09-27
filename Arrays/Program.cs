fc760d0d6ad06c2544ea752f26f499563983fdf1
bptrager <brian.trager@rit.edu>
2021-09-27 09:11:28 -0400

    Update 9/27

---
+++ Arrays/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("How many elements do you want in your fancy new array?: ");
+            //int numOfElements = int.Parse(Console.ReadLine());
+
+            //int[] numbers = new int[numOfElements];
+
+            int[] numbers = new int[5];
+
+            numbers[0] = 3;
+            numbers[1] = 4;
+            numbers[2] = 27;
+            numbers[3] = 5;
+            numbers[4] = 7;
+
+
+            Console.WriteLine($"Index 0: {numbers[0]}");
+            Console.WriteLine($"Index 1: {numbers[1]}");
+            Console.WriteLine($"Index 2: {numbers[2]}");
+            Console.WriteLine($"Index 3: {numbers[3]}");
+            Console.WriteLine($"Index 4: {numbers[4]}");
+
+            Console.WriteLine($"The sum of every element in the array: " +
+                $"{numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4]}");
+
+            Console.WriteLine($"The average of every element in the array: " +
+                $"{(numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4]) / numbers.Length}");
+
+            //array to store string objects
+            string[] strings = new string[3];
+
+            strings[0] = "3";
+            strings[1] = "4";
+            strings[2] = "5";
+
+            Console.WriteLine($"The sum of every element in the string array: " +
+                $"{int.Parse(strings[0]) + int.Parse(strings[1]) + int.Parse(strings[2])}");
+        }
+    }
+}
--- Mock1_WingsHeaven/Program.cs
+++ Mock1_WingsHeaven/Program.cs
@@ -26,13 +26,73 @@ namespace Mock1Exam_WingsHeaven
             const double WAFFLE_FRIES_PRICE = 3.49;
             const double ONION_RINGS_PRICE = 2.49;
 
+            double subTotal = 0;
+            const double TAX_RATE = 0.08;
+            string receipt = "";
+
             Console.WriteLine("*** HEAVENLY WINGS ***\n");
 
-            Console.Write($"Type of Wings:\n\t{"1. Boneless",-20}{BONELESS_WINGS_PRICE, 9:C}" +
-                $"\t{"2. Traditional",-20}{TRADITIONAL_WINGS_PRICE,9:C}\n" +
-                $"\t{"1. Both",-20}{BONELESS_WINGS_PRICE + TRADITIONAL_WINGS_PRICE,9:C}" +
-                $"Enter your selection: ");
+            //First Menu
+            Console.Write($"Type of Wings:\n\t{"1. Boneless",-16}{BONELESS_WINGS_PRICE, 9:C}" +
+                $"\n\t{"2. Traditional",-16}{TRADITIONAL_WINGS_PRICE,9:C}" +
+                $"\n\t{"3. Both",-16}{BONELESS_WINGS_PRICE + TRADITIONAL_WINGS_PRICE,9:C}" +
+                $"\nEnter your selection: ");
 
+            switch(Console.ReadLine())
+            {
+                case "1": subTotal += BONELESS_WINGS_PRICE;
+                    receipt += $"{"Boneless",-25}{BONELESS_WINGS_PRICE,-9:C}";
+                    break;
+                case "2": subTotal += TRADITIONAL_WINGS_PRICE;
+                    receipt += $"{"Traditional",-25}{TRADITIONAL_WINGS_PRICE,-9:C}";
+                    break;
+                case "3": subTotal += BONELESS_WINGS_PRICE + TRADITIONAL_WINGS_PRICE;
+                    receipt += $"{"Both Wings",-25}{BONELESS_WINGS_PRICE + TRADITIONAL_WINGS_PRICE,-9:C}";
+                    subTotal -= (BONELESS_WINGS_PRICE + TRADITIONAL_WINGS_PRICE) - COMBO_WINGS_PRICE;
+                    receipt += $"{"\nDiscount",-25}{((BONELESS_WINGS_PRICE + TRADITIONAL_WINGS_PRICE) - COMBO_WINGS_PRICE) * -1,-9:C}";
+                    break;
+            }
+
+            //Second menu
+            Console.Write($"\n\nType of Sauce:\n\t{"1. Buffalo Sauce",-16}{BUFFALO_SAUCE_PRICE,9:C}" +
+                $"\n\t{"2. Mild Sauce",-16}{MILD_SAUCE_PRICE,9:C}" +
+                $"\n\t{"3. BBQ Sauce",-16}{BBQ_SAUCE_PRICE,9:C}" +
+                $"\n\t{"4. Plain",-16}{"No Charge",9}" +
+                $"\nEnter your selection: ");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    subTotal += BUFFALO_SAUCE_PRICE;
+                    break;
+                case "2":
+                    subTotal += MILD_SAUCE_PRICE;
+                    break;
+                case "3":
+                    subTotal += BBQ_SAUCE_PRICE;
+                    break;
+            }
+
+            //Third Menu
+            Console.Write($"\n\nDo you want a side order:\n\t{"1. Waffle Fries",-16}{WAFFLE_FRIES_PRICE,9:C}" +
+                $"\n\t{"2. Onion Rings",-16}{ONION_RINGS_PRICE,9:C}" +
+                $"\n\t{"3. Both",-16}{WAFFLE_FRIES_PRICE + ONION_RINGS_PRICE,9:C}" +
+                $"\nEnter your selection: ");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    subTotal += WAFFLE_FRIES_PRICE;
+                    break;
+                case "2":
+                    subTotal += ONION_RINGS_PRICE;
+                    break;
+                case "3":
+                    subTotal += WAFFLE_FRIES_PRICE + ONION_RINGS_PRICE;
+                    break;
+            }
+
+            Console.WriteLine(receipt);
 
         }
     }
