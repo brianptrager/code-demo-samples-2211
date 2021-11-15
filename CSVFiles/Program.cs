a51a733375aeef00ba6024e7062ed21dcb00604b
bptrager <brian.trager@rit.edu>
2021-11-15 09:56:54 -0500

    11/15/2021

---
+++ CSVFiles/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CSVFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = null;
+            List<Student> students = new List<Student>();
+            string filePath = @"C:\Users\bptnbs\Documents\roster.csv";
+            string title = "Next Generation Student Information System";
+
+            string[] menuOptions = { "Add student data", "Save data to file", "Load data into program", "Modify student data", "Exit" };
+
+            Console.Title = title;
+            Console.WriteLine(title);
+
+            while (true)
+            {
+                for (int i = 1; i <= menuOptions.Length; i++)
+                {
+                    Console.WriteLine($"{i--}. {menuOptions[i++]}");
+                }
+                Console.Write("Enter your option: ");
+
+                switch (Console.ReadLine())
+                {
+                    case "1": //Add student data
+                        students = AddData(students);
+                        break;
+
+                    case "2": //Save data to file
+                        
+                            WritetoCSV(students, filePath);
+                        break;
+
+                    case "3": //Load data into program
+                        ReadFromCSV(filePath, ref students);
+                        break;
+
+                    case "4": //Modify student data
+                        if (students == null || students.Count == 0)
+                            ReadFromCSV(filePath, ref students);
+
+                        for (int i = 1; i <= students.Count; i++)
+                        {
+                            Console.WriteLine($"{i--}. {students[i++]}");
+                        }
+
+                        Console.Write("Enter number to modify data: ");
+                        int selectedData = int.Parse(Console.ReadLine());
+
+                        Console.Write("Enter the new data: ");
+                        students[--selectedData] = new Student() { Name = Console.ReadLine() };
+                        break;
+
+                    default: //Quit even if it's not a valid choice
+                        Environment.Exit(0);
+                        break;
+                }
+            }//infinite loop
+
+
+            //ReadFromCSV(filePath);
+
+        }
+
+        public static void WritetoCSV(List<Student> students, string path)
+        {
+            bool writeFlag = true;
+            if (File.Exists(path))
+            {
+                Console.Write("Do you wish to overwrite? (y/n): ");
+                if (Console.ReadLine() == "y")
+                    writeFlag = false;
+            }
+
+            StreamWriter writer = new StreamWriter(path, writeFlag);
+
+            foreach (Student s in students)
+            {
+                writer.Write($"{s.Name},");
+            }
+
+            writer.Close();
+        }
+
+        public static void ReadFromCSV(string path, ref List<Student> students)
+        {
+            StreamReader reader = new StreamReader(path);
+            string data = reader.ReadLine();
+            reader.Close();
+
+            if (data != null)
+            {
+                if (data.Contains(','))
+                {
+
+                    string[] splitData = data.Split(',');
+                    int counter = 0;
+
+                    foreach (var sd in splitData)
+                    {
+                        if (sd == "" || sd == null)
+                        {
+                            Console.WriteLine($"{++counter}. No data");
+                            continue;
+                        }
+                        students.Add(new Student() { Name = sd });
+                        Console.WriteLine($"{++counter}. {sd}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("This is not a valid CSV file.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Data is empty or corrupted.");
+            }
+        }//end ReadFromCSV
+
+        public static List<Student> AddData(List<Student> students)
+        {
+            while (true)
+            {
+                Console.Write("Enter name of student: ");
+                students.Add(new Student() { Name = Console.ReadLine() });
+
+                if (Console.ReadLine() == "quit")
+                    break;
+            }
+
+            return students;
+
+            //Write to CSV
+           // WritetoCSV(students, path);
+        }
+    }
+
+    class Student
+    {
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ ReadingFromFiles/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.IO;
+
+namespace ReadingFromFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Ultimate File System for Dummies");
+            string[] menuOptions = { "Create a file", "Read a file", "Edit a file", "Delete a file", "Exit" };
+            string targetDirectory = @"C:\Users\bptnbs\Documents\";
+            string fileName = null;
+            string fullPath = null;
+            
+
+            while (true)
+            {
+                int menuOptionNum = 1;
+
+                foreach (string mo in menuOptions)
+                {
+                    Console.WriteLine($"{menuOptionNum}. {mo}");
+                    menuOptionNum++;
+                }
+                Console.Write("\nEnter your menu option: ");
+
+
+                switch(Console.ReadLine())
+                {
+                    case "1": //Create a file
+                        fullPath = CreateFilePath(targetDirectory);
+
+                        if (!File.Exists(fullPath))
+                        {
+                            using (StreamWriter writer = new StreamWriter(fullPath))
+                            {
+                                Console.Write("");
+                            }
+                        }
+                        else
+                            Console.WriteLine("File already exists, creation skipped.");
+                        break;
+
+                    case "2": //Read a file
+                        fullPath = CreateFilePath(targetDirectory);
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        if (File.Exists(fullPath))
+                        {
+                            using (StreamReader reader = new StreamReader(fullPath))
+                            {
+                                while(!reader.EndOfStream)
+                                {
+                                    Console.WriteLine(reader.ReadLine());
+                                }
+                            }
+                            
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Error: File ({fileName}) does not exist in this directory ({targetDirectory}).");
+                        }
+                        Console.ResetColor();
+                        break;
+
+                    case "3": //Edit a file
+                        fullPath = CreateFilePath(targetDirectory);
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        if (File.Exists(fullPath))
+                        {
+                            using (StreamReader reader = new StreamReader(fullPath))
+                            {
+                                while (!reader.EndOfStream)
+                                {
+                                    Console.WriteLine(reader.ReadLine());
+                                }
+                            }
+
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Error: File ({fileName}) does not exist in this directory ({targetDirectory}).");
+                        }
+
+                        Console.ResetColor();
+                        using(StreamWriter writer = new StreamWriter(fullPath, true))
+                        {
+                            Console.Write("Enter the text you wish to add: ");
+                            Console.ForegroundColor = ConsoleColor.Cyan;
+                            writer.WriteLine($"\n{Console.ReadLine()}");
+                        }
+                        Console.ResetColor();
+                        break;
+
+                    case "4": //Delete a file
+                        fullPath = CreateFilePath(targetDirectory);
+                        if (File.Exists(fullPath))
+                        {
+                            File.Delete(fullPath);
+                            Console.WriteLine($"File deleted: {fullPath}");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"File not found or has already been deleted.");
+                        }
+                        break;
+                    
+                    default:
+                        Environment.Exit(0);
+                        break;
+
+                }
+
+            } //end while(true)
+        }//end Main
+
+        public static string CreateFilePath(string targetDirectory)
+        {
+            Console.Write("Enter the file name: ");
+            string fileName = Console.ReadLine();
+            string fullPath = targetDirectory + fileName;
+
+            return fullPath;
+        }
+    }
+}
